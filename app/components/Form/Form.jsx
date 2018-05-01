6daeeb5f1f48f9c2840fd8d0a45d5f12a3230931
Jeff Chen <>
2018-05-01 00:36:44 -0700

    Added fields

--- app/components/Form/Form.jsx
+++ app/components/Form/Form.jsx
@@ -4,15 +4,66 @@ import TextField from 'material-ui/TextField';
 class Form extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      number: null,
+      firstName: null, 
+      lastName: null,
+      exp_month: null,
+      exp_year: null,
+      cvc: null
+    }
+  }
+
+  renderName() {
+    return (
+      <div>
+        <TextField
+          hintText="First Name"
+          errorText=""
+        />
+        <TextField
+          hintText="Last Name"
+          errorText=""
+        />
+      </div>
+    )
+  }
+
+  renderCreditCardNumber() {
+    return (
+      <div>
+      <TextField
+        hintText="Credit Card Number"
+        errorText="The error text can be as long as you want, it will wrap."
+      />
+      </div>
+    )
+  }
+
+  renderCreditCardInfo() {
+    return (
+      <div>
+        <TextField
+          hintText="Expiration"
+          errorText=""
+        />
+        <TextField
+          hintText="CVV"
+          errorText=""
+        />
+      </div>
+    )
   }
 
   
   render() {
     return(
       <div>
-        <TextField
-        hintText="Hint Text"
-        /><br />
+        {this.renderName()}
+        <br />
+        { this.renderCreditCardNumber() }
+        <br />
+        { this.renderCreditCardInfo() }
       </div>
     )
   }
